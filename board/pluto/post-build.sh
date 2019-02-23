7b465cf2bd216911da12b6da4a8c890bd02425fb
Gwenhael <gwe@trabucayre.com>
2019-02-23 10:09:24 +0100

    board:pluto: generate missing LICENSE.html based on template (list of packages not supported yet).

--- board/pluto/post-build.sh
+++ board/pluto/post-build.sh
@@ -3,6 +3,8 @@
 # $2    board name
 set -e
 
+BOARD_DIR=$(dirname ${0})
+
 # Add a console on tty1
 grep -qE '^ttyGS0::' ${TARGET_DIR}/etc/inittab || \
 sed -i '/GENERIC_SERIAL/a\
@@ -14,6 +16,21 @@ sed -i '/hostname/a\
 
 sed -i -e '/::sysinit:\/bin\/hostname -F \/etc\/hostname/d' ${TARGET_DIR}/etc/inittab
 
+# Prepare LICENSE.html
+if [ ! -e ${BINARIES_DIR}/msd ]; then
+	mkdir ${BINARIES_DIR}/msd
+fi
+
+cp ${BOARD_DIR}/LICENSE.template ${BINARIES_DIR}/msd/LICENSE.html
+cp ${BOARD_DIR}/msd/index.html ${BINARIES_DIR}/msd/
+LINUX_VERS=$(cat ${BR2_CONFIG} | grep '^BR2_LINUX_KERNEL_VERSION' | cut -d\" -f 2)
+UBOOT_VERS=$(cat ${BR2_CONFIG} | grep '^BR2_TARGET_UBOOT_VERSION' | cut -d\" -f 2)
+FW_VERSION=$(cd ${BOARD_DIR} && git describe --abbrev=4 --dirty --always --tags)
+sed -i s/##DEVICE_FW##/${FW_VERSION}/g ${BINARIES_DIR}/msd/LICENSE.html
+sed -i s/##LINUX_VERSION##/${LINUX_VERS}/g ${BINARIES_DIR}/msd/LICENSE.html
+sed -i s/##UBOOT_VERSION##/${UBOOT_VERS}/g ${BINARIES_DIR}/msd/LICENSE.html
+
+
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-msd.cfg"
@@ -24,10 +41,10 @@ rm -rf "${GENIMAGE_TMP}"
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BOARD_DIR}/msd"  \
+	--inputpath "${BINARIES_DIR}/msd"  \
 	--outputpath "${TARGET_DIR}/opt/" \
 	--config "${GENIMAGE_CFG}"
 
 rm -f ${TARGET_DIR}/opt/boot.vfat
 rm -f ${TARGET_DIR}/etc/init.d/S99iiod
-
+rm -rf ${BINARIES_DIR}/msd
