bd91bb8f78ea666a580e46d8cb9bf79d6fdebb12
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-06-06 15:35:41 +0200

    package: pluto-system-top-bit: fetch archive to obtain system_top.bit instead of versionning this file

---
+++ package/pluto-system-top-bit/pluto-system-top-bit.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# pluto-system-top-bit
+#
+################################################################################
+
+PLUTO_SYSTEM_TOP_BIT_VERSION = v0.30
+PLUTO_SYSTEM_TOP_BIT_SOURCE = plutosdr-jtag-bootstrap-$(PLUTO_SYSTEM_TOP_BIT_VERSION).zip
+PLUTO_SYSTEM_TOP_BIT_SITE = http://github.com/analogdevicesinc/plutosdr-fw/releases/download/$(PLUTO_SYSTEM_TOP_BIT_VERSION)
+
+PLUTO_SYSTEM_TOP_BIT_INSTALL_IMAGES = YES
+PLUTO_SYSTEM_TOP_BIT_INSTALL_TARGET = NO
+
+define PLUTO_SYSTEM_TOP_BIT_EXTRACT_CMDS
+	$(UNZIP) $(PLUTO_SYSTEM_TOP_BIT_DL_DIR)/$(PLUTO_SYSTEM_TOP_BIT_SOURCE) system_top.bit -d $(@D)
+endef
+
+define PLUTO_SYSTEM_TOP_BIT_INSTALL_IMAGES_CMDS
+	cp $(@D)/system_top.bit $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
