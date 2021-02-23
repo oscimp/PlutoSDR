8fb14ed7aaac090031cb1303a6fbd76301321c35
Jean-Michel Friedt <friedtj@free.fr>
2021-02-23 11:42:40 +0100

    librpitx & rpitx support

--- package/librpitx/librpitx.mk
+++ package/librpitx/librpitx.mk
@@ -3,32 +3,37 @@
 # librpitx
 #
 ################################################################################
-LIBRPITX_VERSION = master
+LIBRPITX_VERSION = b0172f908be0d8a8990c15dd246f1e630823d9d4
 LIBRPITX_SITE = $(call github,F5OEO,librpitx,$(LIBRPITX_VERSION))
 
 LIBRPITX_INSTALL_STAGING = YES
 LIBRPITX_LICENSE = GPL-3.0
 LIBRPITX_LICENSE_FILES = GNU_General_Public_License.txt
 
-define LIBRPITX_CONFIGURE_CMDS
-	echo "CFLAGS=-g -O3 -std=c++11 -I /opt/vc/include " > $(LIBRPITX_DIR)/src/config.mk
-	echo "LDFLAGS= -lm -lrt -lpthread -L/opt/vc/lib -lbcm_host " >> $(LIBRPITX_DIR)/src/config.mk
-	echo "INSTALL=install" >> $(LIBRPITX_DIR)/src/config.mk
-	echo "MAKE=make" >> $(LIBRPITX_DIR)/src/config.mk
-	echo "prefix=/usr" >> $(LIBRPITX_DIR)/src/config.mk
-	echo "SOVERSION=1" >> $(LIBRPITX_DIR)/src/config.mk
-endef
-
 define LIBRPITX_BUILD_CMDS
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/src
 endef
 
-define LIBRPITX_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/librpitx.a $(STAGING_DIR)/usr/lib/librpitx.a
-endef
-
 define LIBRPITX_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/librpitx.a $(STAGING_DIR)/usr/lib/librpitx.a
+	$(INSTALL) -D -m 0644 $(@D)/src/librpitx.so $(STAGING_DIR)/usr/lib/librpitx.so
+	mkdir -p $(STAGING_DIR)/usr/include/librpitx
+	$(INSTALL) -D -m 0644 $(@D)/src/librpitx.h $(STAGING_DIR)/usr/include/librpitx/librpitx.h
+	$(INSTALL) -D -m 0644 $(@D)/src/dma.h $(STAGING_DIR)/usr/include/librpitx/dma.h
+	$(INSTALL) -D -m 0644 $(@D)/src/gpio.h $(STAGING_DIR)/usr/include/librpitx/gpio.h
+	$(INSTALL) -D -m 0644 $(@D)/src/util.h $(STAGING_DIR)/usr/include/librpitx/util.h
+	$(INSTALL) -D -m 0644 $(@D)/src/dsp.h $(STAGING_DIR)/usr/include/librpitx/dsp.h
+	$(INSTALL) -D -m 0644 $(@D)/src/atv.h $(STAGING_DIR)/usr/include/librpitx/atv.h
+	$(INSTALL) -D -m 0644 $(@D)/src/mailbox.h $(STAGING_DIR)/usr/include/librpitx/mailbox.h
+	$(INSTALL) -D -m 0644 $(@D)/src/fmdmasync.h $(STAGING_DIR)/usr/include/librpitx/fmdmasync.h
+	$(INSTALL) -D -m 0644 $(@D)/src/fskburst.h $(STAGING_DIR)/usr/include/librpitx/fskburst.h
+	$(INSTALL) -D -m 0644 $(@D)/src/amdmasync.h $(STAGING_DIR)/usr/include/librpitx/amdmasync.h
+	$(INSTALL) -D -m 0644 $(@D)/src/iqdmasync.h $(STAGING_DIR)/usr/include/librpitx/iqdmasync.h
+	$(INSTALL) -D -m 0644 $(@D)/src/ngfmdmasync.h $(STAGING_DIR)/usr/include/librpitx/ngfmdmasync.h
+	$(INSTALL) -D -m 0644 $(@D)/src/ookburst.h $(STAGING_DIR)/usr/include/librpitx/ookburst.h
+	$(INSTALL) -D -m 0644 $(@D)/src/phasedmasync.h $(STAGING_DIR)/usr/include/librpitx/phasedmasync.h
+	$(INSTALL) -D -m 0644 $(@D)/src/raspberry_pi_revision.h $(STAGING_DIR)/usr/include/librpitx/raspberry_pi_revision.h
+	$(INSTALL) -D -m 0644 $(@D)/src/rpi.h $(STAGING_DIR)/usr/include/librpitx/rpi.h
+	$(INSTALL) -D -m 0644 $(@D)/src/serialdmasync.h $(STAGING_DIR)/usr/include/librpitx/serialdmasync.h
 endef
 
 $(eval $(generic-package))
--- package/rpitx/rpitx.mk
+++ package/rpitx/rpitx.mk
@@ -3,21 +3,13 @@
 # rpitx
 #
 ################################################################################
-RPITX_VERSION = master
+RPITX_VERSION = cce1fe6acf90d4d34ce304aed48fe80ec4ff51e7
 RPITX_SITE = $(call github,F5OEO,rpitx,$(RPITX_VERSION))
 
 RPITX_INSTALL_STAGING = YES
 RPITX_LICENSE = GPL-3.0
 RPITX_LICENSE_FILES = GNU_General_Public_License.txt
-
-define RPITX_CONFIGURE_CMDS
-	echo "CFLAGS=-g -O3 -std=c++11 -I /opt/vc/include " > $(RPITX_DIR)/src/config.mk
-	echo "LDFLAGS= -lm -lrt -lpthread -L/opt/vc/lib -lbcm_host " >> $(RPITX_DIR)/src/config.mk
-	echo "INSTALL=install" >> $(RPITX_DIR)/src/config.mk
-	echo "MAKE=make" >> $(RPITX_DIR)/src/config.mk
-	echo "prefix=/usr" >> $(RPITX_DIR)/src/config.mk
-	echo "SOVERSION=1" >> $(RPITX_DIR)/src/config.mk
-endef
+RPITX_DEPENDENCIES = ft8_lib librpitx
 
 define RPITX_BUILD_CMDS
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/src
